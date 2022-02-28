8c9ca8027f5262fe66b949a30641edf2cf9e0670
Marcio Machado Pereira <marcio.machado.pereira@gmail.com>
2022-02-28 15:28:35 -0300

    Update resnet18 folder to run on CPU.

---
+++ resnet18/builder.mk
@@ -0,0 +1,27 @@
+.PHONY:: all build clean
+
+MODEL ?= resnet18
+MAIN  ?= main
+
+# Toolchain
+CXX =clang++
+CXXFLAGS = -O2 -Wall
+LIBS := -lpng
+
+BUNDLE_DIR := ./bin
+BUILD_DIR := ./bin
+SRC_DIR := ./
+
+build: $(BUILD_DIR)/$(MAIN).x
+
+$(BUILD_DIR)/$(MAIN).x: $(SRC_DIR)/$(MAIN).o $(BUNDLE_DIR)/$(MODEL).o
+	$(CXX) -o $@ $^ $(LIBS) $(CXXFLAGS)
+
+$(SRC_DIR)/$(MAIN).o : $(SRC_DIR)/$(MAIN).cpp $(BUNDLE_DIR)/$(MODEL).h
+	$(CXX) $< -c -o $@ -I$(BUNDLE_DIR) $(CXXFLAGS)
+
+clean:
+	rm -rf $(BUILD_DIR)/$(MAIN).x
+	rm -rf $(SRC_DIR)/$(MAIN).o
+
+
